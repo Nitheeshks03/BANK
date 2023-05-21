5a0aa34c0c7f95fbb8af6ebdf156053ae5091c41
nitheesh <nitheesh@pop-os.localdomain>
2023-05-21 10:04:49 +0530

    bank

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  uname: any  
+  constructor(private ds:DataService) { }
+  ngOnInit(): void {
+
+  }
+  login() {
+
+    // alert(this.ds.sdata)
+    alert(this.ds.checkData())
+  }
+
+}
